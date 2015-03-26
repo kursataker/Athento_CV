6cbae34b3e8ef9116cc0fdf310717617d08df683
Daniel Ramírez <dramireztorres@gmail.com>
2015-03-26 12:13:11 +0100

    File exists function created

--- utilities/files_management.py
+++ utilities/files_management.py
@@ -1,1 +1,10 @@
-__author__ = 'danielramirez'
+import os
+
+# Library of functions
+
+def file_exists(input_file):
+    """
+    :param input_file: path to the input file
+    :return: true or false wether the file exists or not.
+    """
+    return os.path.isfile(input_file)
