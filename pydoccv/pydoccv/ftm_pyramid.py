27ebd3b732193e4f8312ed903307fa0f43c780fd
Daniel Ramírez <dramireztorres@gmail.com>
2015-04-13 14:22:12 +0200

    FTM Update - array shape error

--- pydoccv/pydoccv/ftm_pyramid.py
+++ pydoccv/pydoccv/ftm_pyramid.py
@@ -6,42 +6,41 @@ import os
 """
 This script performs a fast template matching algorithm using the OpenCV
 function matchTemplate plus an approximation through pyramid construction to
-improve he performance on larger files.
+improve it's performance on large images.
 """
 
 def buildPyramid(image, max_level):
+
     results = [image]
+    aux = image
 
     for i in range(0,max_level):
-        aux = cv.pyrDown(image)
-        results.insert(0, aux)
+        aux = cv.pyrDown(aux)
+        results = [aux] + results
 
     return results
 
+
 def temp_match(input, template, max_level):
 
-    cv.imshow("src", input)
-    cv.waitKey()
     results = []
 
     source_pyr = buildPyramid(input, max_level)
     template_pyr = buildPyramid(template, max_level)
-    for lvl in range(int(max_level), -1, -1):
+
+    for lvl in range(0, int(max_level), 1):
 
         curr_image = source_pyr[lvl]
         curr_template = template_pyr[lvl]
 
-        print curr_image.shape
-        print curr_template.shape
-        print (curr_image.shape + curr_template.shape)
+        dX = curr_image.shape[1] + 1 - curr_template.shape[1]
+        dY = curr_image.shape[0] + 1 - curr_template.shape[0]
 
-        result = np.zeros(np.size(curr_image) +
-                          np.size(1) -
-                          np.size(curr_template),
-                          cv.CV_32FC1)
+        result = np.zeros([dX, dY])
+
 
         #On the first level performs regular template matching.
-        if lvl == max_level:
+        if lvl == 0:
             result = cv.matchTemplate(curr_image, curr_template,
                                       cv.TM_CCORR_NORMED)
 
@@ -49,11 +48,8 @@ def temp_match(input, template, max_level):
         #matching on the predefined ROI areas, obtained using the result of the
         #previous level.
         else:
+            mask = cv.pyrUp(r)
 
-            mask = cv.pyrUp(aux)
-            cv.imshow("Mask", mask)
-            cv.waitKey()
-            #mask8u = cv.cvtColor(mask, cv.CV_32SC1)
             mask8u = cv.inRange(mask, 0, 255)
             contours = cv.findContours(mask8u, cv.RETR_EXTERNAL,
                                        cv.CHAIN_APPROX_NONE)
@@ -61,32 +57,23 @@ def temp_match(input, template, max_level):
             #Uses contours to define the region of interest and perform TM on
             #the areas.
 
-            for i in range(0, np.size(contours)):
+            for i in range(0, np.size(contours)-1):
                 x, y, w, h = cv.boundingRect(contours[i][0])
-                print contours[i][0]
-                print x
-                print y
-                print w
-                print h
-                print cv.boundingRect(contours[i][0])
-                print curr_template.shape
-                #INDEX ERROR, RESULT TIENE DIMENSION 1 (ESCALA DE GRISES)
+                tpl_X = curr_template.shape[1]
+                tpl_Y = curr_template.shape[0]
+
+                #result = cv.matchTemplate(curr_image, curr_template,
+                #                          cv.TM_CCORR_NORMED)
+
                 result[y:y+h, x:x+w] = cv.matchTemplate(
-                                curr_image[
-                                    #x:x+w,
-                                    #y:y+h
-                                    x + (curr_template.shape[1]-1):x+w + (curr_template.shape[1]-1),
-                                    y + (curr_template.shape[0]-1):y+h + (curr_template.shape[0]-1)
-                                ],
-                                curr_template,
-                                cv.TM_CCORR_NORMED)
+                                curr_image[y:y+h+tpl_Y, x:x+w+tpl_X],
+                                curr_template, cv.TM_CCORR_NORMED)
 
-        T, aux = cv.threshold(result, 0.94, 1., cv.THRESH_TOZERO)
-        cv.imshow("test", aux)
+        T, r = cv.threshold(result, 0.94, 1., cv.THRESH_TOZERO)
+        cv.imshow("test", r)
         cv.waitKey()
-        results.append(aux)
+        results.append(r)
     return results
-
 
 
 def ftm_pyramid(input_file, template_file, max_level = 5):
@@ -105,18 +92,22 @@ def ftm_pyramid(input_file, template_file, max_level = 5):
 
     tm_results = temp_match(image, template, max_level)
 
+    c = 0
     flag = False
-    while flag == False:
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(tm_results)
-        if max_val > 0.9:
-            cv.rectangle(cv.point(
-                            max_loc.x, template.cols, max_loc.y + template.y),
-                        (255, 0, 0), 2)
 
-            cv.floodFill(tm_results, max_loc, np.ScalarType(0), 0,
-                         np.ScalarType(0.1), np.ScalarType(1.))
+    while flag == False and c < np.size(tm_results):
+        current = tm_results[c]
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(current)
+        if max_val > 0.9:
+            cv.rectangle(img,
+                        max_loc,
+                        (max_loc[0] + template.shape[1],
+                         max_loc[1] + template.shape[0]),
+                        (0,0,255), 2)
         else:
             flag = True
+
+        c = c+1
 
     cv.imshow("Result", img)
     cv.waitKey()
